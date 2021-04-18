b56e407381b49d222a639cc3367617067959ff13
Gustavo Gomes <gustavo@spigo.net>
2021-04-18 00:28:42 -0300

    init

---
+++ Assets/Tecla.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tecla : MonoBehaviour
+{
+    private AudioSource Audio;
+    private void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+    private void OnMouseDown()
+    {
+        if (Audio.isPlaying)
+        {
+            Audio.Stop();
+        }
+        Audio.Play();
+    }    
+}
---
+++ Assets/TrocaSom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrocaSom : MonoBehaviour
+{
+    public AudioClip somASerTocado;
+    public AudioSource[] teclas;
+
+    public void TrocarSom()
+    {
+        foreach (AudioSource CadaTecla in teclas)
+        {
+            CadaTecla.clip = somASerTocado;
+        }
+
+        foreach (GameObject CadaTecla in GameObject.FindGameObjectsWithTag("Teclas"))
+        {
+            CadaTecla.GetComponent<AudioSource>().clip = somASerTocado;
+        }
+
+        //for (int i = 0; i < teclas.Length; i++)
+        //{
+        //    teclas[i].clip = somASerTocado;
+        //}
+    }
+}
