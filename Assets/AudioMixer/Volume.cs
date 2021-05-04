9282f8ad77cbafd3842b41ebfaa0db59bc12abe8
Gustavo Gomes <gustavo@spigo.net>
2021-05-03 22:35:45 -0300

    Audio Mixer

---
+++ Assets/AudioMixer/Volume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    private Slider slider;
+    public string qualVolume;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = PlayerPrefs.GetFloat(qualVolume, 0);
+        audioMixer.SetFloat(qualVolume , slider.value);
+    }
+    public void AlterarVolume()
+    {
+        audioMixer.SetFloat(qualVolume, slider.value);
+        PlayerPrefs.SetFloat(qualVolume, slider.value);
+    }
+
+}
